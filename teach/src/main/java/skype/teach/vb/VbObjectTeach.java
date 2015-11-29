4c361dd6a50fc6dcca2c18566c97b889220b564f
NadezhdaPrilepa <ivanka_2002@mail.ru>
2015-11-29 22:27:25 +0300

    create cycle Objects

---
+++ teach/src/main/java/skype/teach/vb/VbObjectTeach.java
@@ -0,0 +1,15 @@
+package skype.teach.vb;
+
+/**
+ * @author NPrilepa
+ */
+public class VbObjectTeach {
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+        Object obj1 = obj;
+        obj = new Object();
+        obj1 = null;
+
+    }
+}
