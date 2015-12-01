b62090c529c31ec410f81f903f640842c6b69856
NadezhdaPrilepa <ivanka_2002@mail.ru>
2015-12-01 09:11:37 +0300

    create cycle Objects

--- teach/src/main/java/skype/teach/vb/VbObjectTeach.java
+++ teach/src/main/java/skype/teach/vb/VbObjectTeach.java
@@ -5,11 +5,18 @@ package skype.teach.vb;
  */
 public class VbObjectTeach {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Object obj = new Object();
         Object obj1 = obj;
         obj = new Object();
         obj1 = null;
+        String str = obj.toString();
+        System.out.println(obj);
+        String str1 = "Hello " + obj;
+        Class c = obj.getClass();
+        String name = "java.lang.Object";
+        Object obj2 = Class.forName(name).newInstance();
+        System.out.println("obj2.getClass().getName() = " + obj2.getClass().getName());
 
     }
 }
