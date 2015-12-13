bb82ecef8e12647fb5d863cccf774b3fdd08f8e6
NadezhdaPrilepa <ivanka_2002@mail.ru>
2015-12-14 00:51:36 +0300

    JavaBean and Constant teach

---
+++ teach/src/main/java/skype/teach/vb/VbConstantTeach.java
@@ -0,0 +1,20 @@
+package skype.teach.vb;
+
+/**
+ * @author NPrilepa
+ */
+public class VbConstantTeach {
+    public static final int NUMBER_OF_RECORDS;
+
+    static {
+        NUMBER_OF_RECORDS = 100;
+    }
+
+    public static final VbJavaBeanTeach BEAN_TEACH = new VbJavaBeanTeach();
+
+    public static void main(String[] args) {
+        //  NUMBER_OF_RECORDS = 90;
+//        BEAN_TEACH =new VbJavaBeanTeach();
+        BEAN_TEACH.setName("name");
+    }
+}
