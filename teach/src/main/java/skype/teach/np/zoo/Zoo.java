fe0185535464f23da1db09e0a44a3a47d751a9f6
NPrilepa <ivanka_2002@mail.ru>
2015-12-18 12:49:46 +0300

    Create separate test methods

--- teach/src/main/java/skype/teach/np/zoo/Zoo.java
+++ teach/src/main/java/skype/teach/np/zoo/Zoo.java
@@ -5,24 +5,49 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-/**
+/**class working with animal collection
  * @author NPrilepa
  */
 public class Zoo {
     private Collection zoo = new CopyOnWriteArrayList();
 
+    /**
+     * @return int count of animal collection
+     */
+    public int getAnimalCount() {
+
+        return this.zoo.size();
+    }
+
+    /**
+     * add animal to animal collection
+     * @param animal
+     */
     public void addAnimal(Animal animal) {
         this.zoo.add(animal);
     }
 
+    /**
+     * remove animal from animal collection
+     * @param animal
+     */
     public void removeAnimal(Animal animal) {
         this.zoo.remove(animal);
     }
 
+    /**
+     * overload method removeAnimal(Animal animal)
+     * if animal = Cat - nothing to do
+     * @param animal
+     */
     public void removeAnimal(Cat animal) {
         //do nothing
     }
 
+    /**
+     * Check if collection of animals contains cat
+     * @return true if cat exists in collection
+     */
     public boolean hasCat() {
         Iterator i = this.zoo.iterator();
         while (i.hasNext()) {
@@ -34,6 +59,11 @@ public class Zoo {
         return false;
     }
 
+    /**
+     * Remove from animal collection sick animal.
+     * Animal is sick if its voice is not correct, else
+     * sick() return null and animal stay in animal collection
+     */
     public void removeSickAnimal() {
         Iterator i = this.zoo.iterator();
         while (i.hasNext()) {
@@ -44,13 +74,19 @@ public class Zoo {
         }
     }
 
-
+    /**
+     * Clear all animals from collection
+     */
     public void closeZoo()
 
     {
         this.zoo.clear();
     }
 
+    /**
+     * Check, if all animals of animal collection is Pet, return true
+     * @return
+     */
     public boolean isPetZoo() {
         Iterator i = zoo.iterator();
         while (i.hasNext()) {
@@ -62,7 +98,10 @@ public class Zoo {
         return true;
     }
 
-
+    /**
+     * concat to String voices of all members animal collection
+     * @return String like "Zoo voice is: 'gav' 'meau'", if there is no members in collection return "Zoo voice is:"
+     */
     public String zooVoice() {
         String result = "Zoo voice is:";
         Iterator i = zoo.iterator();
@@ -74,6 +113,10 @@ public class Zoo {
         return result;
     }
 
+    /**
+     * concat to String eat of all members animal collection
+     * @return String like "Zoo eat is: meat milk", if there is no members in collection return "Zoo eat is:"
+     */
     public String zooEat() {
         String result = "Zoo eat is:";
         Iterator i = zoo.iterator();
--- teach/src/test/java/skype/teach/np/zoo/ZooTest.java
+++ teach/src/test/java/skype/teach/np/zoo/ZooTest.java
@@ -9,49 +9,103 @@ import static org.junit.Assert.*;
  */
 public class ZooTest {
     Zoo zooBerlin = new Zoo();
-    Zoo zooMoscow = new Zoo();
+
     Wolf wf = new Wolf();
     Dog dog = new Dog();
     Cat cat = new Cat();
 
-
     @Test
-    public void zooTest() {
+    public void addAnimalTest() {
+        zooBerlin.addAnimal(cat);
+        assertSame("zooBerlin.addAnimal()", 1, zooBerlin.getAnimalCount());
+    }
+    @Test
+    public void removeAnimalTest() {
+        zooBerlin.addAnimal(dog);
+        zooBerlin.removeAnimal(dog);
+        assertSame("zooBerlin.removeAnimal()", 0, zooBerlin.getAnimalCount());
+        zooBerlin.removeAnimal(dog);
+        assertSame("zooBerlin.removeAnimal()", 0, zooBerlin.getAnimalCount());
+    }
+    @Test
+    public void removeAnimalCatTest() {
+        zooBerlin.addAnimal(cat);
+        zooBerlin.removeAnimal(cat);
+        assertSame("zooBerlin.removeAnimal()", 1, zooBerlin.getAnimalCount());
+        zooBerlin.removeAnimal((Animal) cat);
+        assertSame("zooBerlin.removeAnimal()", 0, zooBerlin.getAnimalCount());
 
+    }
+    @Test
+    public void isPetZooTest() {
+        zooBerlin.addAnimal(cat);
+        assertEquals("zooBerlin.isPetZoo()", true, zooBerlin.isPetZoo());
+        zooBerlin.addAnimal(wf);
+        assertEquals("zooBerlin.isPetZoo()", false, zooBerlin.isPetZoo());
+        zooBerlin.addAnimal(dog);
+        assertEquals("zooBerlin.isPetZoo()", false, zooBerlin.isPetZoo());
+    }
+    @Test
+    public void zooVoiceTest() {
         zooBerlin.addAnimal(wf);
         zooBerlin.addAnimal(dog);
         zooBerlin.addAnimal(cat);
-
-        assertEquals("zooBerlin.isPetZoo()", false, zooBerlin.isPetZoo());
+        assertEquals("zooBerlin.zooVoice()", "Zoo voice is: 'u-u-u' 'gav' 'meau'", zooBerlin.zooVoice());
         zooBerlin.removeAnimal(wf);
+        assertEquals("zooBerlin.zooVoice()", "Zoo voice is: 'gav' 'meau'", zooBerlin.zooVoice());
         zooBerlin.removeAnimal(dog);
-        assertEquals("zooBerlin.isPetZoo()", true, zooBerlin.isPetZoo());
-        cat.setVoice("gav");
-        assertEquals("zooBerlin.zooVoice()", "Zoo voice is: 'gav'", zooBerlin.zooVoice());
-        assertEquals("zooBerlin.zooEat()", "Zoo eat is: milk", zooBerlin.zooEat());
-        zooBerlin.closeZoo();
-
-        zooMoscow.addAnimal(wf);
-        zooMoscow.addAnimal(dog);
-        zooMoscow.addAnimal(cat);
-
-        assertEquals("zooMoscow.isPetZoo()", false, zooMoscow.isPetZoo());
-        assertEquals("zooMoscow.zooVoice()", "Zoo voice is: 'u-u-u' 'gav' 'gav'", zooMoscow.zooVoice());
-        assertEquals("zooMoscow.zooEat()", "Zoo eat is: meat dog food milk", zooMoscow.zooEat());
-        zooMoscow.removeAnimal(cat);
-        assertEquals("zooMoscow.getCat()", true, zooMoscow.hasCat());
-        zooMoscow.removeSickAnimal();
-        assertEquals("zooMoscow.getCat()", false, zooMoscow.hasCat());
-        assertEquals("zooMoscow.zooEat()", "Zoo eat is: meat dog food", zooMoscow.zooEat());
-        wf.setVoice("gav");
-        zooMoscow.removeSickAnimal();
-        assertEquals("zooMoscow.isPetZoo()", true, zooMoscow.isPetZoo());
-        dog.setVoice("meau");
-        zooMoscow.addAnimal(cat);
-        cat.setVoice("meau");
-        zooMoscow.removeSickAnimal();
-        zooMoscow.closeZoo();
+        assertEquals("zooBerlin.zooVoice()", "Zoo voice is: 'meau'", zooBerlin.zooVoice());
     }
+    @Test
+    public void zooEatTest() {
+        zooBerlin.addAnimal(wf);
+        zooBerlin.addAnimal(dog);
+        zooBerlin.addAnimal(cat);
+        assertEquals("zooBerlin.zooVoice()", "Zoo eat is: meat dog food milk", zooBerlin.zooEat());
+        zooBerlin.removeAnimal((Animal)cat);
+        assertEquals("zooBerlin.zooVoice()", "Zoo eat is: meat dog food", zooBerlin.zooEat());
+        zooBerlin.removeAnimal(wf);
+        assertEquals("zooBerlin.zooVoice()", "Zoo eat is: dog food", zooBerlin.zooEat());
 
+    }
+    @Test
+    public void hasCatTest() {
+        zooBerlin.addAnimal(wf);
+        assertEquals("zooMoscow.getCat()", false, zooBerlin.hasCat());
+        zooBerlin.addAnimal(cat);
+        assertEquals("zooMoscow.getCat()", true, zooBerlin.hasCat());
+        zooBerlin.removeAnimal(cat);
+        assertEquals("zooMoscow.getCat()", true, zooBerlin.hasCat());
+        zooBerlin.removeAnimal((Animal) cat);
+        assertEquals("zooMoscow.getCat()", false, zooBerlin.hasCat());
+    }
+    @Test
+      public void removeSickAnimalTest() {
+        zooBerlin.addAnimal(wf);
+        zooBerlin.addAnimal(dog);
+        zooBerlin.addAnimal(cat);
+        wf.setVoice("gav");
+        zooBerlin.removeSickAnimal();
+        assertSame("zooBerlin.removeSickAnimal()", 2, zooBerlin.getAnimalCount());
+        cat.setVoice("gav");
+        zooBerlin.removeSickAnimal();
+        assertSame("zooBerlin.removeSickAnimal()", 1, zooBerlin.getAnimalCount());
+    }
+    @Test
+    public void removeSickAnimalTest_() {
+        zooBerlin.addAnimal(wf);
+        zooBerlin.addAnimal(dog);
+        zooBerlin.addAnimal(cat);
+        zooBerlin.removeSickAnimal();
+        assertSame("zooBerlin.removeSickAnimal()", 3, zooBerlin.getAnimalCount());
+    }
+    @Test
+    public void closeZooTest() {
+        zooBerlin.addAnimal(wf);
+        zooBerlin.addAnimal(dog);
+        zooBerlin.addAnimal(cat);
+        zooBerlin.closeZoo();
+        assertSame("zooBerlin.closeZoo()",0, zooBerlin.getAnimalCount());
+    }
 
 }
