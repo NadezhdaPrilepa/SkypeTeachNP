11b2303c802edf07195215e7e85688a7f995f611
NPrilepa <ivanka_2002@mail.ru>
2015-12-23 18:27:56 +0300

    create cycle for tests on method isValid()

--- teach/src/test/java/skype/teach/np/registration/NPRegistrationTest.java
+++ teach/src/test/java/skype/teach/np/registration/NPRegistrationTest.java
@@ -13,52 +13,36 @@ public class NPRegistrationTest {
 
     @Test
     public void registerUser_login_incorrect() {
-        assertNull("login must be incorrect", npr.registerUser("NP", "q1w2e3r4t5"));
-        assertNull("login must be incorrect", npr.registerUser(null, "q1w2e3r4t5"));
-        assertNull("login must be incorrect", npr.registerUser("", "q1w2e3r4t5"));
-        assertNull("login must be incorrect", npr.registerUser(" ", "q1w2e3r4t5"));
-        assertNull("login must be incorrect", npr.registerUser("@NPrilepa", "q1w2e3r4t5"));
-        assertNull("login must be incorrect", npr.registerUser("%NPrilepa", "q1w2e3r4t5"));
+        assertNull("login must be incorrect: registerUser(\"NP\", \"q1w2e3r4t5\")", npr.registerUser("NP", "q1w2e3r4t5"));
+        assertNull("login must be incorrect: registerUser(null, \"q1w2e3r4t5\")", npr.registerUser(null, "q1w2e3r4t5"));
+        assertNull("login must be incorrect: registerUser(\"\", \"q1w2e3r4t5\")", npr.registerUser("", "q1w2e3r4t5"));
+        assertNull("login must be incorrect: registerUser(\" \", \"q1w2e3r4t5\")", npr.registerUser(" ", "q1w2e3r4t5"));
+        assertNull("login must be incorrect: registerUser(\"@NPrilepa\", \"q1w2e3r4t5\")", npr.registerUser("@NPrilepa", "q1w2e3r4t5"));
+        assertNull("login must be incorrect: registerUser(\"%NPrilepa\", \"q1w2e3r4t5\")", npr.registerUser("%NPrilepa", "q1w2e3r4t5"));
 
     }
 
     @Test
     public void registerUser_pass_incorrect() {
-        assertNull("Pass must be incorrect", npr.registerUser("NPrilepa", "q1w2"));
-        assertNull("Pass must be incorrect", npr.registerUser("NPrilepa", null));
-        assertNull("Pass must be incorrect", npr.registerUser("NPrilepa", ""));
-        assertNull("Pass must be incorrect", npr.registerUser("NPrilepa", " "));
+        assertNull("Pass must be incorrect: registerUser(\"NPrilepa\", \"q1w2\")", npr.registerUser("NPrilepa", "q1w2"));
+        assertNull("Pass must be incorrect: registerUser(\"NPrilepa\", null)", npr.registerUser("NPrilepa", null));
+        assertNull("Pass must be incorrect: registerUser(\"NPrilepa\", \"\")", npr.registerUser("NPrilepa", ""));
+        assertNull("Pass must be incorrect: registerUser(\"NPrilepa\", \" \")", npr.registerUser("NPrilepa", " "));
     }
 
     @Test
     public void registerUser_login_and_pass_incorrect() {
-        assertNull("Pass and Login must be incorrect", npr.registerUser(null, null));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("NP", null));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("", null));
-        assertNull("Pass and Login must be incorrect", npr.registerUser(" ", null));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("@NPrilepa", null));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("%NPrilepa", null));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("", ""));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("NP", ""));
-        assertNull("Pass and Login must be incorrect", npr.registerUser(null, ""));
-        assertNull("Pass and Login must be incorrect", npr.registerUser(" ", ""));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("@NPrilepa", ""));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("%NPrilepa", ""));
-        assertNull("Pass and Login must be incorrect", npr.registerUser(" ", " "));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("NP", " "));
-        assertNull("Pass and Login must be incorrect", npr.registerUser(null, " "));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("", " "));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("@NPrilepa", " "));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("%NPrilepa", " "));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("NP", "q1w2"));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("@NPrilepa", "q1w2"));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("%NPrilepa", "q1w2"));
-        assertNull("Pass and Login must be incorrect", npr.registerUser(" ", "q1w2"));
-        assertNull("Pass and Login must be incorrect", npr.registerUser(null, "q1w2"));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("", "q1w2"));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("@NPrilepa", "q1w2"));
-        assertNull("Pass and Login must be incorrect", npr.registerUser("%NPrilepa", "q1w2"));
-
+        String[] incorrectLogin ={null,"NP",""," ","@NPrilepa","%NPrilepa"};
+        String[] incorrectPass = {null,""," ","q1w2"};
+        String exceptionStr;
+        for (int i=0;i<incorrectLogin.length-1;i++)
+        {
+            for (int j=0;j<incorrectPass.length-1;j++)
+            {
+                exceptionStr="Pass and Login must be incorrect: login='"+incorrectLogin[i]+"' pass='"+incorrectPass[j]+"'";
+                assertNull(exceptionStr,npr.registerUser(incorrectLogin[i], incorrectPass[j]));
+            }
+        }
     }
 
     @Test
--- teach/src/test/java/skype/teach/np/registration/NpValidatorTest.java
+++ teach/src/test/java/skype/teach/np/registration/NpValidatorTest.java
@@ -2,6 +2,8 @@ package skype.teach.np.registration;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -12,51 +14,35 @@ public class NpValidatorTest {
 
     @Test
     public void isValid_login_incorrect() {
-        assertFalse("login must be incorrect", npv.isValid("NP", "q1w2e3r4t5"));
-        assertFalse("login must be incorrect", npv.isValid(null, "q1w2e3r4t5"));
-        assertFalse("login must be incorrect", npv.isValid("", "q1w2e3r4t5"));
-        assertFalse("login must be incorrect", npv.isValid(" ", "q1w2e3r4t5"));
-        assertFalse("login must be incorrect", npv.isValid("@NPrilepa", "q1w2e3r4t5"));
-        assertFalse("login must be incorrect", npv.isValid("%NPrilepa", "q1w2e3r4t5"));
+        assertFalse("login must be incorrect: isValid(\"NP\", \"q1w2e3r4t5\")", npv.isValid("NP", "q1w2e3r4t5"));
+        assertFalse("login must be incorrect: isValid(null, \"q1w2e3r4t5\")", npv.isValid(null, "q1w2e3r4t5"));
+        assertFalse("login must be incorrect: isValid(\"\", \"q1w2e3r4t5\")", npv.isValid("", "q1w2e3r4t5"));
+        assertFalse("login must be incorrect: isValid(\" \", \"q1w2e3r4t5\")", npv.isValid(" ", "q1w2e3r4t5"));
+        assertFalse("login must be incorrect: isValid(\"@NPrilepa\", \"q1w2e3r4t5\")", npv.isValid("@NPrilepa", "q1w2e3r4t5"));
+        assertFalse("login must be incorrect: isValid(\"%NPrilepa\", \"q1w2e3r4t5\")", npv.isValid("%NPrilepa", "q1w2e3r4t5"));
     }
 
     @Test
     public void isValid_pass_incorrect() {
-        assertFalse("Pass must be incorrect", npv.isValid("NPrilepa", "q1w2"));
-        assertFalse("Pass must be incorrect", npv.isValid("NPrilepa", null));
-        assertFalse("Pass must be incorrect", npv.isValid("NPrilepa", ""));
-        assertFalse("Pass must be incorrect", npv.isValid("NPrilepa", " "));
+        assertFalse("Pass must be incorrect: isValid(\"NPrilepa\", null)", npv.isValid("NPrilepa", null));
+        assertFalse("Pass must be incorrect: isValid(\"NPrilepa\", \"\")", npv.isValid("NPrilepa", ""));
+        assertFalse("Pass must be incorrect: isValid(\"NPrilepa\", \" \")", npv.isValid("NPrilepa", " "));
 
     }
 
     @Test
     public void isValid_login_and_pass_incorrect() {
-        assertFalse("Pass and Login must be incorrect", npv.isValid(null, null));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("NP", null));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("", null));
-        assertFalse("Pass and Login must be incorrect", npv.isValid(" ", null));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("@NPrilepa", null));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("%NPrilepa", null));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("", ""));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("NP", ""));
-        assertFalse("Pass and Login must be incorrect", npv.isValid(null, ""));
-        assertFalse("Pass and Login must be incorrect", npv.isValid(" ", ""));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("@NPrilepa", ""));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("%NPrilepa", ""));
-        assertFalse("Pass and Login must be incorrect", npv.isValid(" ", " "));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("NP", " "));
-        assertFalse("Pass and Login must be incorrect", npv.isValid(null, " "));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("", " "));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("@NPrilepa", " "));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("%NPrilepa", " "));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("NP", "q1w2"));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("@NPrilepa", "q1w2"));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("%NPrilepa", "q1w2"));
-        assertFalse("Pass and Login must be incorrect", npv.isValid(" ", "q1w2"));
-        assertFalse("Pass and Login must be incorrect", npv.isValid(null, "q1w2"));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("", "q1w2"));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("@NPrilepa", "q1w2"));
-        assertFalse("Pass and Login must be incorrect", npv.isValid("%NPrilepa", "q1w2"));
+        String[] incorrectLogin ={null,"NP",""," ","@NPrilepa","%NPrilepa"};
+        String[] incorrectPass = {null,""," ","q1w2"};
+        String exceptionStr;
+        for (int i=0;i<incorrectLogin.length-1;i++)
+        {
+            for (int j=0;j<incorrectPass.length-1;j++)
+            {
+                exceptionStr="Pass and Login must be incorrect: login='"+incorrectLogin[i]+"' pass='"+incorrectPass[j]+"'";
+                assertFalse(exceptionStr, npv.isValid(incorrectLogin[i], incorrectPass[j]));
+            }
+        }
     }
 
     @Test
